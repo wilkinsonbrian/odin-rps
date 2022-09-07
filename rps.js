1e6ac5ef41b23d571baf22852d890c938523be7d
brianwilkinson <wilkibrian@gmail.com>
2022-09-07 14:22:11 -0400

    Rough outline of RPS UI version. Unstyled

--- rps.js
+++ rps.js
@@ -1,6 +1,32 @@
+let compChoice = "";
+let userChoice = "";
+let userScore = 0;
+let compScore = 0;
+let remove = false;
+
+function removeListeners() {
+    const butts = document.querySelectorAll('button');
+    butts.forEach(butt => butt.removeEventListener('click', getUserChoice));
+}
 function getComputerChoice() {
     const choices = ["rock", "paper", "scissors"];
-    return choices[Math.floor(Math.random() * choices.length)];
+    compChoice = choices[Math.floor(Math.random() * choices.length)];
+}
+
+function updateScore() {
+    const resultDiv = document.querySelector('.results');
+    if (userScore == 5) {
+        resultDiv.textContent = "Game Over: You beat the computer."
+        removeListeners();
+    }
+    if (compScore == 5) {
+        resultDiv.textContent = "Game Over: The computer beat you."
+        removeListeners();
+    }
+    const player = document.querySelector(".pScoreValue");
+    const computer = document.querySelector(".cScoreValue");
+    player.textContent = userScore;
+    computer.textContent = compScore;
 }
 
 function playRound(playerChoice, computerChoice) {
@@ -8,57 +34,49 @@ function playRound(playerChoice, computerChoice) {
     const result2 = "Scissors beats Paper.";
     const result3 = "Rock beats Scissors.";
 
+    const resultDiv = document.querySelector('.results');
+    
+
+
     playerChoice = playerChoice.toLowerCase();
     if (playerChoice == "rock" && computerChoice == "paper") {
-        console.log("You lose! " + result1);
-        return 0;
+        resultDiv.textContent = "You lose! " + result1;
+        compScore++;
     } else if (playerChoice == "rock" && computerChoice == "scissors") {
-        console.log("You win! " + result3);
-        return 1;
+        resultDiv.textContent = "You win! " + result3;
+        userScore++;
     } else if (playerChoice == "paper" && computerChoice == "scissors") {
-        console.log("You lose! " + result2);
-        return 0;
+        resultDiv.textContent = "You lose! " + result2;
+        compScore++;
     } else if (playerChoice == "paper" && computerChoice == "rock") {
-        console.log("You win! " + result1);
-        return 1;
+        resultDiv.textContent = "You win! " + result1;
+        userScore++;
     } else if (playerChoice == "scissores" && computerChoice == "rock") {
-        console.log("You lose! " + result3);
-        return 0;
+        resultDiv.textContent = "You lose! " + result3;
+        compScore++;
     } else if (playerChoice == "scissors" && computerChoice == "paper") {
-        console.log("You win! " + result2);
-        return 1; 
+        resultDiv.textContent = "You win! " + result2;
+        userScore++; 
     } else {
-        console.log("It's a tie. You both chose " + playerChoice);
-        return 2;
+        resultDiv.textContent = "It's a tie. You both chose " + playerChoice;
     }
+
+    updateScore();
 }
 
 function getUserChoice() {
-    while (true) {
-        let user = prompt("Do you choose rock, paper, or scissors?");
-        user = user.toLowerCase();
-        if (user == "rock" || user == "paper" || user == "scissors") {
-            return user;
-        }
-        console.log("That is not a valid answer.");
-    }
-    
+    if (userScore == 5 || compScore == 5) return;
+    userChoice = this.value;
+    getComputerChoice();
+    playRound(userChoice, compChoice);
+
 }
 
 function game() {
-    let userScore = 0;
-    let compScore = 0;
-    for (let i=0; i<5; i++) {
-        let user = getUserChoice();
-        let computer = getComputerChoice();
-        let winner = playRound(user, computer);
-        if (winner == 1) {
-            userScore++;
-        } else if (winner == 0) {
-            compScore++;
-        }
-    }
-    console.log("You won " + userScore + " games, and the computer won " + compScore + " games.");
+
+    const butts = document.querySelectorAll('button');
+
+    butts.forEach(butt => butt.addEventListener('click', getUserChoice));
     
 }
 
